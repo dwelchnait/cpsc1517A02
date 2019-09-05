077fc89d4b11d3625d79312bff7fbb0dba0b19a9
Don Welch <dwelch@nait.ca>
2019-09-05 13:46:43 -0600

    create first object

---
+++ OOPsSolution/OOPsReview/FencePanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    //the default permissions is private
+    //if an outside user of this class wanted
+    //  access to the class contents; then the
+    //  class permissions must be public
+    public class FencePanel
+    {
+
+    }
+}

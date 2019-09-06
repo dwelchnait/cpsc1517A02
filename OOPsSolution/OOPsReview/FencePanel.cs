4a1f872302e7e3f3978fb3f3e8e39f55b3cc0677
Don Welch <dwelch@nait.ca>
2019-09-06 13:38:37 -0600

    introduction to objects
    cover object
    cover data member
    cover property

--- OOPsSolution/OOPsReview/FencePanel.cs
+++ OOPsSolution/OOPsReview/FencePanel.cs
@@ -12,6 +12,79 @@ namespace OOPsReview
     //  class permissions must be public
     public class FencePanel
     {
+        //Properties
+        //property is associated with a single piece of data
+        //property has two sub components
+        //    get: returns a value to the calling agent
+        //    set: receives a value from the calling agent
+        //         the keyword use to represent the  incoming data
+        //         is value
+        //a property DOES NOT have a developer's parameter
+
+        //two ways to code a property
+        //Auto-Implemented
+        //  a private data member DOES NOT need to be coded
+        //  the system will create an internal data member that
+        //      the sytem will manage
+        public double Height { get; set; }
+        //public double Width { get; set; }
+
+        //Fully Implemented
+        //  a private data member WILL be coded for use by this property
+        //  typically the incoming data needs additional processing
+        //     such as validation
+        //  Exaample: the characteristic is a string that can be null or
+        //            requires at least on character (that is the string
+        //            can not be empty)
+
+        private string _Style;
+        private double _Width;
+
+        public string Style
+        {
+            get
+            {
+                return _Style;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Style = null;
+                }
+                else
+                {
+                    _Style = value;
+                }
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return _Width;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    new Exception("Width can not be 0 or less than 0.");
+                }
+                else
+                {
+                    _Width = value;
+                }
+            }
+        }
+
+        //Does a nullable non-string data value need a fully-Implemented
+        //    property?
+        //NO. a nullable numeric will either be given a numeric value OR null;
+        //However: if the numeric needs additional checking then you should
+        //         consider using a fully-implemented property
+        public double? Price { get; set; }
+
 
     }
 }

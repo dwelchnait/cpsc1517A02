b8ec426bbb6a65b2a23da35285b9acc6f01bc410
Don Welch <dwelch@nait.ca>
2019-10-15 13:14:12 -0600

    Create new asp.net web app
    investigate asp.net web application, create hello world page

---
+++ BasicAspNet/WebApp/SamplePages/HelloWorld.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Hello World" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" 
+    CodeBehind="HelloWorld.aspx.cs" Inherits="WebApp.SamplePages.HelloWorld" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Hello World</h1>
+    <asp:Label ID="HelloText" runat="server" Text="Hi there, welcome to the world of easy."></asp:Label>
+    <asp:Button ID="Submit" runat="server" Text="Submit" OnClick="Submit_Click" />
+</asp:Content>
---
+++ BasicAspNet/WebApp/SamplePages/HelloWorld.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            HelloText.Text = "Boo !";
+            HelloText.Font.Size = 100;
+            HelloText.ForeColor = System.Drawing.Color.Crimson;
+        }
+    }
+}

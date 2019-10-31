5c7c99bf9dc324448ff7e287e6bcc152ab8204da
Don Welch <dwelch@nait.ca>
2019-10-31 13:17:47 -0600

    implemented Submit event

--- BasicAspNet/WebApp/SamplePages/JobApplication.aspx.cs
+++ BasicAspNet/WebApp/SamplePages/JobApplication.aspx.cs
@@ -26,7 +26,39 @@ namespace WebApp.SamplePages
 
         protected void Submit_Click(object sender, EventArgs e)
         {
-            Message.Text = "you pressed the Submit button";
+            //gather the entered data and display the values
+
+            //need to collecct the entered data
+            //string fullname = FullName.Text;
+            string email = EmailAddress.Text;
+            string phone = PhoneNumber.Text;
+            string time = FullOrPartTime.SelectedValue;
+
+            //create a message string containing the entered data
+            string msg = string.Format("Name: {0} Email: {1} Phone: {2} Time: {3}",
+                                FullName.Text, email, phone, time);
+
+            //to handle the checkbox list, traverse the list
+            //     and obtain the dta that was selected
+            //during the traverse, create a string of jobs selected
+            //after the traverse, add the string of jobs OR an error msg to
+            //  the other message data string
+            string jobs = " Jobs: ";
+            bool found = false;
+            foreach(ListItem item in Jobs.Items)
+            {
+                if(item.Selected)
+                {
+                    found = true;
+                    jobs += item.Value + " ";
+                }
+            }
+            if (!found)
+            {
+                jobs += " you did not select a job. Application Rejected.";
+            }
+            //display the message string in the output message control
+            Message.Text = msg + jobs;
         }
     }
 }

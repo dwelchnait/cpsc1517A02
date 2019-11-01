26d3dfcf10fa1ec3ec0e1ea8ddd7ca787feebecf
Don Welch <dwelch@nait.ca>
2019-11-01 12:20:20 -0600

    Create CEntry.cs

---
+++ BasicAspNet/WebApp/CEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+    public class CEntry
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2 { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string EmailAddress { get; set; }
+
+        public CEntry()
+        {
+
+        }
+        public CEntry(string firstname, string lastname,
+                      string streetaddress1, string streetaddress2,
+                      string city, string province,
+                      string postalcode, string email)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            StreetAddress1 = streetaddress1;
+            StreetAddress2 = streetaddress2;
+            City = city;
+            Province = province;
+            PostalCode = postalcode;
+            EmailAddress = email;
+        }
+    }
+}
